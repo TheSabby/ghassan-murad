550e69a4f102fd296a3097170a6b068d9dc9b154
Musaab Abdalla <musabelfaki@gmail.com>
2017-10-20 22:12:49 +0300

    transition to css grid

---
+++ projects/projects.js
@@ -0,0 +1,800 @@
+var projects = [
+  {
+    id: 1,
+    title: 'Baghdad Olympics',
+    description:
+      'Baghdad 2040 Olympics is a virtual project dipicting the brand and visual identity of the olympic games if they were to be hosted in Baghdad in 2040, including concepts and pictograms along with several unconventional advertisement posters.',
+    images: [
+      {
+        id: 1,
+        image: '01.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '1/3'
+          }
+        }
+      },
+      {
+        id: 2,
+        image: '02.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 3,
+        image: '03.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '2/4'
+          }
+        }
+      },
+      {
+        id: 4,
+        image: '04.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 5,
+        image: '05.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 6,
+        image: '06.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '1/3'
+          }
+        }
+      },
+      {
+        id: 7,
+        image: '07.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 8,
+        image: '08.jpg',
+        tile: {
+          large: {
+            rows: '2/4',
+            cols: '4/5'
+          }
+        }
+      }
+    ],
+    showcase_image: '01.jpg',
+    storage_path: 'baghdad_olympics',
+    tile: {
+      id: {
+        large: {
+          rows: '1/2',
+          cols: '1/3'
+        }
+      }
+    },
+    type: 'branding',
+    year: 2014
+  },
+  {
+    id: 2,
+    title: 'Yazan Haifawy - Aman Aman',
+    showcase_image: 'Aman.jpg',
+    storage_path: 'aman_aman',
+    tile: {
+      id: {
+        large: {
+          rows: '5/6',
+          cols: '2/4'
+        }
+      }
+    },
+    type: 'video',
+    video_url: 'VMyqus2UnxM',
+    year: 2016
+  },
+  {
+    id: 3,
+    title: 'Barista Nitro Cold Brew Coffee',
+    showcase_image: 'Nitro_Cold_Brew.jpg',
+    storage_path: 'barista_nitro_cold_brew',
+    tile: {
+      id: {
+        large: {
+          rows: '3/4',
+          cols: '3/4'
+        }
+      }
+    },
+    type: 'video',
+    video_url: 'pexn6Q1yjYU',
+    year: 2017
+  },
+  {
+    id: 4,
+    title: 'Confused App Promo',
+    showcase_image: 'Confused.jpg',
+    storage_path: 'confused_app',
+    tile: {
+      id: {
+        large: {
+          rows: '2/3',
+          cols: '2/3'
+        }
+      }
+    },
+    type: 'video',
+    video_url: '8_PlIBfPF_Y',
+    year: 2013
+  },
+  {
+    id: 5,
+    title: "Spray n' Play",
+    description:
+      'The concept behind this advertising/marketing campaign does not only promote LEGOs mission statement, ìInspire and develop the builders of tomorrow but also provokes consumers to initiate a referral network marketing campaign. The main idea was to ignite a promotional event that is of such simplicity to impact consumers themselves to spread exposure. The subjects are to get a construction block, doesn’t matter the size. However, the bigger the better. With the use of spray paint (or any other form of paint) the subjects are to paint the construction block then get a free LEGO Spray n’ Play sticker from the nearest Lego distributer, stick it on the block, take photos of the painted block with the sticker in an interesting spot, post the photo on facebook with the hashtag #spraynplay and the subject with an interesting photo would be contacted to receive a gift from LEGO.',
+    images: [
+      {
+        id: 1,
+        image: '00.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '1/3'
+          }
+        }
+      },
+      {
+        id: 2,
+        image: '01.jpg',
+        tile: {
+          large: {
+            rows: '1/3',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 3,
+        image: '02.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 4,
+        image: '03.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 5,
+        image: '04.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 6,
+        image: '05.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 7,
+        image: '06.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '1/3'
+          }
+        }
+      },
+      {
+        id: 8,
+        image: '07.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '3/5'
+          }
+        }
+      }
+    ],
+    showcase_image: '00.jpg',
+    storage_path: 'lego_spray_n_play',
+    tile: {
+      id: {
+        large: {
+          rows: '1/2',
+          cols: '3/4'
+        }
+      }
+    },
+    type: 'advertising',
+    year: 2015
+  },
+  {
+    id: 6,
+    title: 'Man vs Nature Narration',
+    description: '',
+    images: [
+      {
+        id: 1,
+        image: '0.jpg',
+        tile: {
+          large: {
+            rows: '1/5',
+            cols: '1/3'
+          }
+        }
+      },
+      {
+        id: 2,
+        image: '1.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 3,
+        image: '2.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 4,
+        image: '3.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 5,
+        image: '4.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 6,
+        image: '5.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 7,
+        image: '6.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 8,
+        image: '7.jpg',
+        tile: {
+          large: {
+            rows: '4/5',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 9,
+        image: '8.jpg',
+        tile: {
+          large: {
+            rows: '4/5',
+            cols: '4/5'
+          }
+        }
+      }
+    ],
+    showcase_image: '3.jpg',
+    storage_path: 'man_vs_nature_narration',
+    tile: {
+      id: {
+        large: {
+          rows: '4/5',
+          cols: '2/3'
+        }
+      }
+    },
+    type: 'branding',
+    year: 2013
+  },
+  {
+    id: 7,
+    title: 'Youth Knowledge Forum',
+    description:
+      'Youth Knowledge Forum is an annual activity presented by Majdi Bukhari International that takes place in Al-Madinah in Saudi Arabia. The forum promotes several subjects to the youth in different fields, including Mobile Applications, Cloud Computing, Virtual Reality and 3D printing. It also focuses on the evolving the Information Technology sector in Al-Madinah in regard to shifting the IT community and covers the most modern practices in the field. The work involved the creation of the event brand, advertising concepts and posters, in addition to the design elements inside the forum.',
+    images: [
+      {
+        id: 1,
+        image: '1.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 2,
+        image: '2.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 3,
+        image: '3.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 4,
+        image: '4.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 5,
+        image: '5.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '1/3'
+          }
+        }
+      },
+      {
+        id: 6,
+        image: '6.jpg',
+        tile: {
+          large: {
+            rows: '1/3',
+            cols: '4/5'
+          }
+        }
+      }
+    ],
+    showcase_image: '5.jpg',
+    storage_path: 'youth_knowledge_forum',
+    tile: {
+      id: {
+        large: {
+          rows: '2/3',
+          cols: '3/5'
+        }
+      }
+    },
+    type: 'branding',
+    year: 2017
+  },
+  {
+    id: 8,
+    title: 'Jadal Kanz',
+    showcase_image: 'Kanz.jpg',
+    storage_path: 'jadal_kanz',
+    tile: {
+      id: {
+        large: {
+          rows: '1/2',
+          cols: '4/5'
+        }
+      }
+    },
+    type: 'video',
+    video_url: '_b6TcXfLKlc',
+    year: 2016
+  },
+  {
+    id: 9,
+    title: 'Jadal - Mish Hadol Nasak',
+    showcase_image: 'Mish_Hadol_Nasak.jpg',
+    storage_path: 'jadal_mish_hadol_nasak',
+    tile: {
+      id: {
+        large: {
+          rows: '5/6',
+          cols: '4/5'
+        }
+      }
+    },
+    type: 'video',
+    video_url: 'LIWMTrInE0Y',
+    year: 2016
+  },
+  {
+    id: 10,
+    title: 'G12 Line',
+    description:
+      'G12 T-Shirt Line is a virtual clothing line brand, designing funky high quality T-Shirts, The brand identity focuses on being up-to-date with the youth scene trends. G12 also provides the service of printing custom designs on T-Shirts.',
+    images: [
+      {
+        id: 1,
+        image: '01.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 2,
+        image: '02.jpg',
+        tile: {
+          large: {
+            rows: '1/3',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 3,
+        image: '03.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '1/3'
+          }
+        }
+      },
+      {
+        id: 4,
+        image: '04.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 5,
+        image: '05.jpg',
+        tile: {
+          large: {
+            rows: '1/3',
+            cols: '4/5'
+          }
+        }
+      }
+    ],
+    showcase_image: '01.jpg',
+    storage_path: 'g12_line',
+    tile: {
+      id: {
+        large: {
+          rows: '4/6',
+          cols: '1/2'
+        }
+      }
+    },
+    type: 'branding',
+    year: 2015
+  },
+  {
+    id: 11,
+    title: 'Wahj Mall',
+    description:
+      'As Ludwig Mies van der Rohe has stated ‘Less is more’. The Seagram Building located in Park Avenue is a reflection of simplicity and integrity in form and structure. Wahj Mall combines the international style through Mies’ modern stylistic movement and the luxury depicted in the of simplicity of design, for the customers to be in an environment that would amplify the ultimate shopping experience. The virtual work involved the creation of the brand, and the signage inside the building.',
+    images: [
+      {
+        id: 1,
+        image: '01.jpg',
+        tile: {
+          large: {
+            rows: '1/3',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 2,
+        image: '02.jpg',
+        tile: {
+          large: {
+            rows: '1/3',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 3,
+        image: '03.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 4,
+        image: '04.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 5,
+        image: '05.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 6,
+        image: '06.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 7,
+        image: '07.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 8,
+        image: '08.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 9,
+        image: '09.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 10,
+        image: '10.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '3/4'
+          }
+        }
+      }
+    ],
+    showcase_image: '01.jpg',
+    storage_path: 'wahj_mall',
+    tile: {
+      id: {
+        large: {
+          rows: '2/3',
+          cols: '1/2'
+        }
+      }
+    },
+    type: 'branding',
+    year: 2015
+  },
+  {
+    id: 12,
+    title: 'Electronic Services Forum',
+    description:
+      'Electronic Services Forum is an annual activity presented by Majdi Bukhari International that takes place in Al-Madinah in Saudi Arabia. The forum discusses E-Commerce and E-Governmental Services for both individuals and businesses. It also focuses on the implementation of a technological and legal infrastructure in order to launch these services. The work involved the creation of the event brand, in addition to the design elements inside the forum.',
+    images: [
+      {
+        id: 1,
+        image: '1.jpg',
+        tile: {
+          large: {
+            rows: '1/2',
+            cols: '1/5'
+          }
+        }
+      },
+      {
+        id: 2,
+        image: '2.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 3,
+        image: '3.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 4,
+        image: '4.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '2/3'
+          }
+        }
+      },
+      {
+        id: 5,
+        image: '5.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '1/2'
+          }
+        }
+      },
+      {
+        id: 6,
+        image: '6.jpg',
+        tile: {
+          large: {
+            rows: '2/4',
+            cols: '4/5'
+          }
+        }
+      },
+      {
+        id: 7,
+        image: '7.jpg',
+        tile: {
+          large: {
+            rows: '2/3',
+            cols: '3/4'
+          }
+        }
+      },
+      {
+        id: 8,
+        image: '8.jpg',
+        tile: {
+          large: {
+            rows: '3/4',
+            cols: '3/4'
+          }
+        }
+      }
+    ],
+    showcase_image: '1.jpg',
+    storage_path: 'electronic_services_forum',
+    tile: {
+      id: {
+        large: {
+          rows: '3/4',
+          cols: '1/3'
+        }
+      }
+    },
+    type: 'branding',
+    year: 2013
+  },
+  {
+    id: 13,
+    title: 'Pill In Ibiza',
+    showcase_image: 'Ibiza.jpg',
+    storage_path: 'pill_in_ibiza',
+    tile: {
+      id: {
+        large: {
+          rows: '4/5',
+          cols: '3/5'
+        }
+      }
+    },
+    type: 'video',
+    video_url: '0_btxzHnCUk',
+    year: 2016
+  },
+  {
+    id: 14,
+    title: 'Supercharge',
+    showcase_image: 'Supercharge.jpg',
+    storage_path: 'supercharge',
+    tile: {
+      id: {
+        large: {
+          rows: '3/4',
+          cols: '4/5'
+        }
+      }
+    },
+    type: 'video',
+    video_url: 'amprNk-mnvc',
+    year: 2013
+  }
+];
